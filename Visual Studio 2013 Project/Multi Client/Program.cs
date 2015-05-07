136864a84a0b79e7e538cb2969a16403b0ab7d67
muhammad.umair@tu-ilmenau.de <muhammad.umair@tu-ilmenau.de>
2015-05-07 03:38:59 +0200

    Visual Studio Projects
    MultiClient Console App, Multi Server Console App, Windows Server VS
    2013 projects are added.
    
    - Multi Client Creates sockets and listens to the MultiServer Socket,
    which can send data to any of the client sockets.
    
    - Windows Service Code will be updated from Multi-Client, with LogNet ,
    ZeroMQ Nuget Packages

---
+++ Visual Studio 2013 Project/Multi Client/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Collections.Generic;
+
+
+
+
+namespace Multi_Client
+{
+    class Program
+    {
+
+        private static readonly List<Socket> _clientSockets = new List<Socket>();
+        //private static readonly Socket _clientSocket = new Socket (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private const int _PORT = 100;
+        private const int _BUFFER_SIZE = 2048;
+        private static readonly byte[] _buffer = new byte[_BUFFER_SIZE];
+        static void Main()
+        {
+            Console.Title = "Client";
+            InitializeAndConnect();
+            //RequestLoop();
+            string x = Console.ReadLine();
+            Exit();
+        }
+
+        private static void InitializeAndConnect()
+        {
+            Socket A = null, B=null, C=null, D=null, E=null, F=null;
+
+            _clientSockets.Add(A);
+            _clientSockets.Add(B);
+            _clientSockets.Add(C);
+            _clientSockets.Add(D);
+            _clientSockets.Add(E);
+            _clientSockets.Add(F);
+
+            int index = _clientSockets.Count;
+            while(index !=0)
+            { 
+                index = index -1;
+                ConnectToServer(index);
+            }
+        }
+
+        private static void ConnectToServer(int sockNumber)
+        {
+            int attempts = 0;
+                _clientSockets[sockNumber] = new Socket (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            while (!_clientSockets[sockNumber].Connected)
+            {
+                try
+                {
+                    attempts++;
+                    Console.WriteLine("Connection attempt " + attempts);
+                    _clientSockets[sockNumber].Connect(IPAddress.Loopback, _PORT);
+                    _clientSockets[sockNumber].BeginReceive(_buffer, 0, _BUFFER_SIZE, SocketFlags.None, ReceiveCallback, _clientSockets[sockNumber]);
+                }
+                catch (SocketException) 
+                {
+                    Console.Clear();
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Connected");
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket current = (Socket)AR.AsyncState;
+            int received;
+
+            try
+            {
+                received = current.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Client forcefully disconnected");
+                current.Close(); // Dont shutdown because the socket may be disposed and its disconnected anyway
+                _clientSockets.Remove(current);
+                return;
+            }
+
+            byte[] recBuf = new byte[received];
+            Array.Copy(_buffer, recBuf, received);
+            string text = Encoding.ASCII.GetString(recBuf);
+            Console.WriteLine("Received Text at: " + text);
+
+            //if (text.ToLower() == "get time") // Client requested time
+            //{
+            //    Console.WriteLine("Text is a get time request");
+            //    byte[] data = Encoding.ASCII.GetBytes(DateTime.Now.ToLongTimeString());
+            //    current.Send(data);
+            //    Console.WriteLine("Time sent to client");
+            //}
+            //else if (text.ToLower() == "exit") // Client wants to exit gracefully
+            //{
+            //    // Always Shutdown before closing
+            //    current.Shutdown(SocketShutdown.Both);
+            //    current.Close();
+            //    _clientSockets.Remove(current);
+            //    Console.WriteLine("Client disconnected");
+            //    return;
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Text is an invalid request");
+            //    byte[] data = Encoding.ASCII.GetBytes("Invalid request");
+            //    current.Send(data);
+            //    Console.WriteLine("Warning Sent");
+            //}
+            current.BeginReceive(_buffer, 0, _BUFFER_SIZE, SocketFlags.None, ReceiveCallback, current);
+        }
+        private static void RequestLoop()
+        {
+            Console.WriteLine(@"<Type ""exit"" to properly disconnect client>");
+            //while (true)
+            //{
+            //    //SendRequest();
+            //    //ReceiveResponse();
+            //}
+        }
+
+        /// <summary>
+        /// Close socket and exit app
+        /// </summary>
+        private static void Exit()
+        {
+            SendString("exit"); // Tell the server we re exiting
+            for (int i = 0; i < _clientSockets.Count; i++)
+            {
+                _clientSockets[i].Shutdown(SocketShutdown.Both);
+                _clientSockets[i].Close();               
+            }
+            Environment.Exit(0);
+        }
+
+        private static void SendRequest()
+        {
+            Console.Write("Send a request: ");
+            string request = Console.ReadLine();
+            SendString(request);
+
+            if (request.ToLower() == "exit")
+            {
+                Exit();
+            }
+        }
+
+        /// <summary>
+        /// Sends a string to the server with ASCII encoding
+        /// </summary>
+        private static void SendString(string text)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(text);
+            _clientSockets[0].Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+        private static void ReceiveResponse()
+        {
+            var buffer = new byte[2048];
+            for (int i = 0; i < _clientSockets.Count; i++)
+            {
+                //int received = _clientSockets[i].Receive(buffer, SocketFlags.None);
+                //if (received == 0) continue;
+                //var data = new byte[received];
+                //Array.Copy(buffer, data, received);
+                //string text = Encoding.ASCII.GetString(data);
+                //Console.WriteLine("Socket#"+i+"::"+text);
+            }
+        }
+    }
+}
---
+++ Visual Studio 2013 Project/Multi Server/Program.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Multi_Server
+{
+    class Program
+    {
+        private static Socket _serverSocket;
+        private static readonly List<Socket> _clientSockets = new List<Socket>();
+        private const int _BUFFER_SIZE = 2048;
+        private const int _PORT = 100;
+        private static readonly byte[] _buffer = new byte[_BUFFER_SIZE];
+
+        static void Main()
+        {
+            Console.Title = "Server";
+            SetupServer();
+           while(true)
+           {
+
+               //string y = Console.WriteLine("1-Unicast (1 to 6 to send message stream to Socket#)\n 2- BroadCast on all Client Sockets");
+               string x = Console.ReadLine(); // When we press enter close everything
+               if(x == "exit")
+                    CloseAllSockets();
+               else if(x == "1")
+               {
+                   while(true)
+                   {
+                       string input = Console.ReadLine();
+                       if (input != "quit")
+                       {
+                           Console.WriteLine("Sending Message to:\n",x);
+                           Send(_clientSockets[Convert.ToInt32(x)-1], input);
+                       }
+                       else
+                       {
+                          break;
+                       }
+                   }
+                   
+
+               }
+               else if (x == "2")
+               {
+                   string input = Console.ReadLine();
+                   if (input != "quit")
+                   {
+                       Console.WriteLine("Sending Message to:\n", x);
+                       Send(_clientSockets[Convert.ToInt32(x) - 1], input);
+                   }
+                   else
+                   {
+                       break;
+                   }
+               }
+               else if (x == "3")
+               {
+                   string input = Console.ReadLine();
+                   if (input != "quit")
+                   {
+                       Console.WriteLine("Sending Message to:\n", x);
+                       Send(_clientSockets[Convert.ToInt32(x) - 1], input);
+                   }
+                   else
+                   {
+                       break;
+                   }
+               }
+               else if (x == "4")
+               {
+                   string input = Console.ReadLine();
+                   if (input != "quit")
+                   {
+                       Console.WriteLine("Sending Message to:\n", x);
+                       Send(_clientSockets[Convert.ToInt32(x) - 1], input);
+                   }
+                   else
+                   {
+                       break;
+                   }
+               }
+               else if (x == "5")
+               {
+                   string input = Console.ReadLine();
+                   if (input != "quit")
+                   {
+                       Console.WriteLine("Sending Message to:\n", x);
+                       Send(_clientSockets[Convert.ToInt32(x) - 1], input);
+                   }
+                   else
+                   {
+                       break;
+                   }
+               }
+               else if (x == "6")
+               {
+                   string input = Console.ReadLine();
+                   if (input != "quit")
+                   {
+                       Console.WriteLine("Sending Message to:\n", x);
+                       Send(_clientSockets[Convert.ToInt32(x) - 1], input);
+                   }
+                   else
+                   {
+                       break;
+                   }
+               }
+           }
+            
+        }
+
+        private static void SetupServer()
+        {
+            Console.WriteLine("Setting up server...");
+            _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any, _PORT));
+            _serverSocket.Listen(100);
+            _serverSocket.BeginAccept(AcceptCallback, null);
+            Console.WriteLine("Server setup complete");
+        }
+
+        /// <summary>
+        /// Close all connected client (we do not need to shutdown the server socket as its connections
+        /// are already closed with the clients)
+        /// </summary>
+        private static void CloseAllSockets()
+        {
+            foreach (Socket socket in _clientSockets)
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+
+            _serverSocket.Close();
+        }
+
+        private static void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket;
+
+            try
+            {
+                socket = _serverSocket.EndAccept(AR);
+            }
+            catch (ObjectDisposedException) // I cannot seem to avoid this (on exit when properly closing sockets)
+            {
+                return;
+            }
+
+           _clientSockets.Add(socket);
+           //socket.BeginReceive(_buffer, 0, _BUFFER_SIZE, SocketFlags.None, ReceiveCallback, socket);
+           Console.WriteLine("Client connected, waiting for request...");
+           _serverSocket.BeginAccept(AcceptCallback, null);
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket current = (Socket)AR.AsyncState;
+            int received;
+
+            try
+            {
+                received = current.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Client forcefully disconnected");
+                current.Close(); // Dont shutdown because the socket may be disposed and its disconnected anyway
+                _clientSockets.Remove(current);
+                return;
+            }
+
+            byte[] recBuf = new byte[received];
+            Array.Copy(_buffer, recBuf, received);
+            string text = Encoding.ASCII.GetString(recBuf);
+            Console.WriteLine("Received Text: " + text);
+
+            if (text.ToLower() == "get time") // Client requested time
+            {
+                Console.WriteLine("Text is a get time request");
+                byte[] data = Encoding.ASCII.GetBytes(DateTime.Now.ToLongTimeString());
+                current.Send(data);
+                Console.WriteLine("Time sent to client");
+            }
+            else if (text.ToLower() == "exit") // Client wants to exit gracefully
+            {
+                // Always Shutdown before closing
+                current.Shutdown(SocketShutdown.Both);
+                current.Close();
+                _clientSockets.Remove(current);
+                Console.WriteLine("Client disconnected");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Text is an invalid request");
+                byte[] data = Encoding.ASCII.GetBytes("Invalid request");
+                current.Send(data);
+                Console.WriteLine("Warning Sent");
+            }
+
+            current.BeginReceive(_buffer, 0, _BUFFER_SIZE, SocketFlags.None, ReceiveCallback, current);
+        }
+
+        private static void Send(Socket handler, String data)
+        {
+            // Convert the string data to byte data using ASCII encoding.
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+            // Begin sending the data to the remote device.
+            handler.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), handler);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the socket from the state object.
+                Socket handler = (Socket)ar.AsyncState;
+
+                // Complete sending the data to the remote device.
+                int bytesSent = handler.EndSend(ar);
+                Console.WriteLine("Sent {0} bytes to client.", bytesSent);
+
+                //handler.Shutdown(SocketShutdown.Both);
+                //handler.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
